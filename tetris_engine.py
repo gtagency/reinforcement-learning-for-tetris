941c57b41b9c57ecf0f4fd23ce5090fb4ba92aae
zchen827 <zchen827@gatech.edu>
2022-01-29 15:17:49 -0500

    horizontal movement for 1 piece

--- tetris_engine.py
+++ tetris_engine.py
@@ -33,8 +33,8 @@ class GameState:
         self.width = width
         self.height = height
         self.gameBoard = [[initVal for col in range(height)] for row in range(width)]
-        self.currPiece = (width, height)
-        self.gameBoard[width-1][height-1] = 1
+        self.currPiece = (int(width/2), int(height-1))
+        self.gameBoard[int(width/2)][height-1] = 1
 
     # TODO:
     # - return view of current game board
@@ -55,15 +55,50 @@ class GameState:
     #  implement the core game logic, with the least code repetition. It will
     #  require the helper below, which I've also described.
     def update(self, action):
-        pass
+        # action should be a tuple(dx, dy)
+        # newX, newY = self.currPiece + action
+        # newPiece = (newX, newY)
+        x, y = action
+        # print(x, y)
+        if self._is_valid_piece_location(self.currPiece, x, y):
+            # print(self.currPiece)
+            # clear the previous block and update the board
+            self.gameBoard[self.currPiece[0]][self.currPiece[1]] = 0
+            # update the current piece position
+            # print(action)
+            self.currPiece = (self.currPiece[0] + x, self.currPiece[1] + y)
+            # print(self.currPiece)
+            self.gameBoard[self.currPiece[0]][self.currPiece[1]] = 1
+            print(self.get_current_board())
+        else:
+            print("Invalid movement")
+
 
     # TODO:
     # - implement this helper that just checks if this is a valid piece location
     # - will need to check for
     #   - out of bounds
-    #   - overlap with existing squares
+    #   - overlap with existing squares (have not being implemented)
     def _is_valid_piece_location(self, piece, x, y):
-        pass
+        row, col = piece
+        # print(row, col, x, y)
+        if row + x < 0 or row + x >= self.width:
+            return False
+        if col + y < 0 or col + y >= self.height:
+            return False
+        return True
 
 game = GameState()
 print(game.get_current_board())
+game.update((-1, 0))
+print()
+game.update((-1, 0))
+print()
+game.update((-1, 0))
+print()
+game.update((-1, 0))
+print()
+game.update((-1, 0))
+print()
+game.update((-1, 0))
+print()
