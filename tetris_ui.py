29b89af35958451aa15cd596c3a266db49c46a3b
Gao, Ruoshi <rgao1@gatech.edu>
2022-02-02 16:23:56 -0500

    Works with game, also no longer anti-gravity

--- tetris_ui.py
+++ tetris_ui.py
@@ -7,7 +7,7 @@ game = GameState()
 pygame.init()
 
 # Sharay: board to be displayed
-board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+"""board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -16,12 +16,12 @@ board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]"""
 
 # Sharay: some basic information, makes adapting the program easier
 pixelSize = 30
-gridx = len(board)
-gridy = len(board[1])
+gridx = game.width
+gridy = game.height
 
 screen = pygame.display.set_mode((pixelSize * gridx, pixelSize * gridy))
 
@@ -33,24 +33,33 @@ action_lookup = {
     pygame.K_s: Action.ROTATE_CCW
     }
 
-while True:
 
+# clock = pygame.time.Clock()
+
+
+while True:
+    # Sharay: this just waits a bit before running, temporary
+    pygame.time.wait(1000)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit(0)
         elif event.type == pygame.KEYDOWN:
-            action = (action_lookup[event.type] if event.type in action_lookup else Action.IDLE)
+            # Sharay: Updates the game with the key action
+            action = (action_lookup[event.key] if event.key in action_lookup else Action.IDLE)
             game.update(action)
+        else :
+            # No key is pressed, should still update game
+            game.update(Action.IDLE)
 
     # cells[x][y] = True
-    # print(game.get_current_board())
+    board = game.gameBoard
 
     # Sharay: alters colors in the board
     screen.fill((0, 0, 0))
     for i in range(len(board)):
         for j in range(len(board[0])):
-            if board[i][j] == 1:
+            if board[i][len(board[0]) - 1 - j] == 1:
                 pygame.draw.rect(screen, (255, 255, 255),
                                  pygame.Rect(pixelSize * i, pixelSize * j, pixelSize, pixelSize))
 
